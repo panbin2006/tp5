8a495da536aaa6d17bd2a3eca931bbd2eb55e206
panbin2006 <502752983@qq.com>
2023-04-21 11:01:56 +0800

    webhook模块更新pull路径为项目根目录

--- application/api/controller/v1/Webhook.php
+++ application/api/controller/v1/Webhook.php
@@ -25,27 +25,21 @@ class Webhook extends Controller
      * 拉取服务器代码
      */
     public  function pull(){
-        // $this->email();
-
-
-
-
         // 获取git服务器提交的数据
         $inputs = input('post.');
-        // $path = "/d/code/PHP/zscd01_tp5_test_webhook"; //项目存放物理路径
+        $path = ROOT_PATH; //项目存放物理路径
         $ref = $inputs['ref'];
         $user = $inputs['pusher']['name'];
         $repository = $inputs['repository']['name'];
         $date = $inputs['head_commit']['timestamp'];
         //判断master分支上是否有提交
         if ($ref=='refs/heads/master') {
-            $cmd = 'd: && cd /code/PHP/zscd01/ && git pull github master 2>&1';
+            $cmd = 'cd  '.$path.'  && git pull gitee master 2>&1';
             exec($cmd,$arr,$result);
             echo '<pre>';
             var_dump($arr);
             var_dump($result);
             Log::write($user.'于'.$date.'向仓库'.$repository.'提交了代码');
-
         }
         echo 'done';
         //发送邮件
@@ -56,43 +50,7 @@ class Webhook extends Controller
         }
     }
 
-    public  function email(){
-        $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-        try {
-            //服务器配置
-            $mail->CharSet ="UTF-8";                     //设定邮件编码
-            $mail->SMTPDebug = 0;                        // 调试模式输出
-            $mail->isSMTP();                             // 使用SMTP
-            $mail->Host = 'smtp.qq.com';                // SMTP服务器
-            $mail->SMTPAuth = true;                      // 允许 SMTP 认证
-            $mail->Username = '502752983@qq.com';                // SMTP 用户名  即邮箱的用户名
-            $mail->Password = 'ggfacakeirdhbjag';             // SMTP 密码  部分邮箱是授权码(例如163邮箱)
-            $mail->SMTPSecure = 'ssl';                    // 允许 TLS 或者ssl协议
-            $mail->Port = 465;                            // 服务器端口 25 或者465 具体要看邮箱服务器支持
 
-            $mail->setFrom('502752983@qq.com', 'panbin');  //发件人
-            $mail->addAddress('502752983@qq.com', '安韩');  // 收件人
-            //$mail->addAddress('ellen@example.com');  // 可添加多个收件人
-            //$mail->addReplyTo('xxxx@163.com', 'info'); //回复的时候回复给哪个邮箱 建议和发件人一致
-            //$mail->addCC('cc@example.com');                    //抄送
-            //$mail->addBCC('bcc@example.com');                    //密送
-
-            //发送附件
-            // $mail->addAttachment('../xy.zip');         // 添加附件
-            // $mail->addAttachment('../thumb-1.jpg', 'new.jpg');    // 发送附件并且重命名
-
-            //Content
-            $mail->isHTML(true);                                  // 是否以HTML文档格式发送  发送后客户端可直接显示对应HTML内容
-            $mail->Subject = '这是可达鸭' . time();
-            $mail->Body    = '<h1>可达鸭大葱鸭</h1>' . date('Y-m-d H:i:s');
-            $mail->AltBody = '如果邮件客户端不支持HTML则显示此内容';
-
-            $mail->send();
-            echo '邮件发送成功';
-        } catch (Exception $e) {
-            echo '邮件发送失败: ', $mail->ErrorInfo;
-        }
-    }
 
     public  function send_email($toemail, $subject, $body) {
         //示例化PHPMailer核心类
